b742b012baba63fe4557ba7113ea070dc499b035
qhayy <saharaputriqhaylha@gmail.com>
2026-06-17 16:30:43 +0800

    Fix register axiosInstance import

--- src/pages/Registrasi.jsx
+++ src/pages/Registrasi.jsx
@@ -1,6 +1,6 @@
 import { useState } from "react";
 import { Link, useNavigate } from "react-router-dom";
-import axios from "axios";
+import axiosInstance from "../api/axiosInstance";
 
 function Registrasi() {
   const navigate = useNavigate();
