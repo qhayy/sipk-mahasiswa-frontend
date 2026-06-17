4293bf79d2ba6eff8a8c74a0de0339a08279fdc8
qhayy <saharaputriqhaylha@gmail.com>
2026-06-17 14:12:10 +0800

    Update frontend API to Railway

--- src/api/axiosInstance.js
+++ src/api/axiosInstance.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const axiosInstance = axios.create({
-  baseURL: "http://localhost:5000",
+  baseURL: "https://sipk-mahasiswa-backend-production.up.railway.app",
 });
 
 axiosInstance.interceptors.request.use(
